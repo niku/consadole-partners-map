99e88a67e1b9f92f972e18318d58b4e4660f2f3e
niku <10890+niku@users.noreply.github.com>
2023-12-10 23:05:00 +0900

    Implement Marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:consadole_partners_map/geocodes_builder.g.dart';
+import 'package:consadole_partners_map/relationship_partner.dart';
+import 'package:consadole_partners_map/relationship_partners_builder.g.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+final PopupController _popupLayerController = PopupController();
 
 void main() {
   runApp(const MyApp());
@@ -34,14 +40,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyFlutterMap(),
+      home: MyMap(),
     );
   }
 }
 
-class MyFlutterMap extends StatelessWidget {
-  MyFlutterMap({super.key});
+class MyMap extends StatefulWidget {
+  const MyMap({super.key});
 
+  @override
+  State<StatefulWidget> createState() => _MyMapState();
+}
+
+class _MyMapState extends State<MyMap> {
   final defaultCenter = const LatLng(43.1, 142.5);
   final double defaultZoom = 8;
   final defaultMaxBounds =
@@ -49,17 +60,45 @@ class MyFlutterMap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Marker> markers = geocodes.entries.map((entry) {
+      return _RelationshipPartnerMarker(
+          relationshipPartners: relationshipPartners
+              .where((element) => entry.value.contains(element.id))
+              .toList(),
+          point: entry.key,
+          child: Builder(
+            builder: (context) => Icon(
+              Icons.location_pin,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ));
+    }).toList();
+
     return FlutterMap(
       options: MapOptions(
-        initialCenter: defaultCenter,
-        initialZoom: defaultZoom,
-        cameraConstraint: CameraConstraint.contain(bounds: defaultMaxBounds),
-      ),
+          initialCenter: defaultCenter,
+          initialZoom: defaultZoom,
+          cameraConstraint: CameraConstraint.contain(bounds: defaultMaxBounds),
+          onTap: (_, __) => _popupLayerController
+              .hideAllPopups(), // Hide popup when the map is tapped.
+          interactionOptions: const InteractionOptions(
+            flags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+          )),
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'com.example.app',
         ),
+        PopupMarkerLayer(
+            options: PopupMarkerLayerOptions(
+                popupController: _popupLayerController,
+                markers: markers,
+                popupDisplayOptions: PopupDisplayOptions(
+                    builder: (BuildContext context, Marker marker) {
+                  marker as _RelationshipPartnerMarker;
+                  return _MyPopup(
+                      relationshipPartners: marker.relationshipPartners);
+                }))),
         RichAttributionWidget(
           attributions: [
             TextSourceAttribution(
@@ -71,5 +110,174 @@ class MyFlutterMap extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class _RelationshipPartnerMarker extends Marker {
+  final List<RelationshipPartner> relationshipPartners;
+
+  const _RelationshipPartnerMarker({
+    required this.relationshipPartners,
+    required super.point,
+    required super.child,
+    super.key,
+    super.width,
+    super.height,
+    super.alignment,
+    super.rotate,
+  }) : super();
+}
+
+class _MyPopup extends StatelessWidget {
+  final List<RelationshipPartner> relationshipPartners;
+
+  const _MyPopup({required this.relationshipPartners});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: relationshipPartners.map((relationshipPartner) {
+              return Table(
+                  border: TableBorder.all(),
+                  defaultColumnWidth: const IntrinsicColumnWidth(),
+                  children: <TableRow>[
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('名称'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(relationshipPartner.name),
+                      )),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('現在のカテゴリー'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(relationshipPartner.rank),
+                      )),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('パートナー継続年数'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(
+                            relationshipPartner.continuationYears.toString()),
+                      )),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('職種'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                              padding: const EdgeInsets.all(2),
+                              child: Text(
+                                relationshipPartner.category,
+                              ))),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('住所'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(relationshipPartner.address),
+                      )),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('現在のカテゴリー'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(relationshipPartner.category),
+                      )),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('電話番号'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(relationshipPartner.phoneNumber),
+                      )),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('Web'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                              padding: const EdgeInsets.all(2),
+                              child: GestureDetector(
+                                onTap: () {
+                                  final url =
+                                      Uri.tryParse(relationshipPartner.url);
+                                  if (url != null) {
+                                    launchUrl(url);
+                                  }
+                                },
+                                child: Text(relationshipPartner.url,
+                                    style: const TextStyle(
+                                      color: Colors.blue,
+                                      decoration: TextDecoration.underline,
+                                    )),
+                              ))),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('会員特典'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(relationshipPartner.benefit),
+                      )),
+                    ]),
+                    TableRow(children: <Widget>[
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text('一言コメント'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(relationshipPartner.comment),
+                      )),
+                    ]),
+                  ]);
+            }).toList()));
   }
 }
